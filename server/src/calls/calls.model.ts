5dd678ce06e36a9c663c975d074f3c62b90088ce
AlekseiYuzhanin <ZombeHalk@gmail.com>
2023-06-20 15:35:31 +0700

    calls model adding

---
+++ server/src/calls/calls.model.ts
@@ -0,0 +1,35 @@
+import { Column, DataType, Table,Model, ForeignKey } from "sequelize-typescript";
+import { Towns } from "src/towns/towns.model";
+import { User } from "src/users/users.model";
+
+interface CallsCreationAttribute{
+    CallId: number;
+    UserId: number;
+    TownId: number;
+    DateOfCalling:Date;
+    CallDuration:number;
+    TimeOfCall:Date;
+}
+
+@Table({tableName: 'calls',createdAt:false,updatedAt:false})
+export class Calls extends Model<Calls,CallsCreationAttribute>{
+    @Column({type: DataType.INTEGER, unique:true,autoIncrement:true, primaryKey:true})
+    CallId: number;
+
+    @ForeignKey(() => User)
+    @Column({type: DataType.INTEGER, unique:true, allowNull:false})
+    UserId:number;
+
+    @ForeignKey(() => Towns)
+    @Column({type: DataType.INTEGER,  allowNull:false})
+    TownId:number;
+
+    @Column({type: DataType.DATE, allowNull:false})
+    DateOfCalling:number;
+
+    @Column({type: DataType.INTEGER,  allowNull:true})
+    CallDuration:number;
+
+    @Column({type: DataType.TIME,  allowNull:true})
+    TimeOfCall:number;
+}
